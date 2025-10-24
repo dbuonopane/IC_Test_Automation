2bfb1f3ef26ccde4ef960bfaa5f11538330b8115
dbuonopane <dbuonopane@gmail.com>
2025-10-24 00:17:45 -0400

    Initial commit - IC test automation project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IC_Test_Automation
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("=== IC Test Automation Framework ===");
+
+            // Initialize Drivers
+            var ps = new PowerSupply();
+            var spi = new SpiInterface();
+
+            // Define Tests
+            var tests = new List<ITest>
+            {
+                new VoltageStabilityTest(ps),
+                new RegisterReadWriteTest(spi)
+            };
+
+            // Run Tests
+            var sequencer = new TestSequencer(tests);
+            sequencer.ExecuteAll();
+
+            // Generate Final Report
+            ReportGenerator.Generate(sequencer.Results);
+       
+
+        }
+    }
+}
